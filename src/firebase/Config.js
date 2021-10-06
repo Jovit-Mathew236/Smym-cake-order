9b9a9c8008f5cd1abe041e19b43635d8021040be
Jovit-Mathew236 <jovitmathew236@gmail.com>
2021-10-06 22:24:43 +0530

    Change the firebase project

--- src/firebase/Config.js
+++ src/firebase/Config.js
@@ -1,51 +1,13 @@
 import firebase from 'firebase'
 
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyBhtoSHB-mqxMukZCXSwiKORQ3XhMyHWxM",
-//   authDomain: "smym-copy-pro.firebaseapp.com",
-//   projectId: "smym-copy-pro",
-//   storageBucket: "smym-copy-pro.appspot.com",
-//   messagingSenderId: "339418728142",
-//   appId: "1:339418728142:web:e203caf0aed0b15f0ab29f"
-// };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCh2Bm_Xy3UHv0Io85K2mKG6F2PFo4QxIw",
-//   authDomain: "smym-cake-order-271d5.firebaseapp.com",
-//   projectId: "smym-cake-order-271d5",
-//   storageBucket: "smym-cake-order-271d5.appspot.com",
-//   messagingSenderId: "809382252747",
-//   appId: "1:809382252747:web:59b12728678abefb788b4e",
-//   measurementId: "G-CEDCC5ECJ2"
-// };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCoey33v5-pZAdDN8nJxT6K7bOoOw8fpy0",
-//   authDomain: "smym-cake-order-finish.firebaseapp.com",
-//   projectId: "smym-cake-order-finish",
-//   storageBucket: "smym-cake-order-finish.appspot.com",
-//   messagingSenderId: "682042258901",
-//   appId: "1:682042258901:web:9a33d918c9fba36dfd395c"
-// };
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB2p0qiNO_OGyz-zl87OjUKMH0zBviPwR8",
-//   authDomain: "smym-cake-order-finish-test.firebaseapp.com",
-//   projectId: "smym-cake-order-finish-test",
-//   storageBucket: "smym-cake-order-finish-test.appspot.com",
-//   messagingSenderId: "454206972901",
-//   appId: "1:454206972901:web:46c8c3f70ddceb6e6c450c"
-// };
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCa5l3Ni7F1QLTx95MKYSjIEOqXsQ_XVXk",
-  authDomain: "fir-60cfe.firebaseapp.com",
-  projectId: "fir-60cfe",
-  storageBucket: "fir-60cfe.appspot.com",
-  messagingSenderId: "149124780801",
-  appId: "1:149124780801:web:18f69436ced26115b18100",
-  measurementId: "G-W7XDT0DV6B"
+  apiKey: "AIzaSyBknoXqlDjp3bWqRwgZid-aZYbLr17trLU",
+  authDomain: "smym-cake-order-30bde.firebaseapp.com",
+  projectId: "smym-cake-order-30bde",
+  storageBucket: "smym-cake-order-30bde.appspot.com",
+  messagingSenderId: "541892635760",
+  appId: "1:541892635760:web:ac5b3b3542af2a1c36d8b5",
+  measurementId: "G-TTLV2430BC"
 };
-
 export default firebase.initializeApp(firebaseConfig)
